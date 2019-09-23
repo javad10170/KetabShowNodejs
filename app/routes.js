0ad315482a7ab2689272d201df1d63408f1719b1
javad gholampoor <javad10170@hotmail.com>
2019-09-23 14:29:32 +0330

    mirror update

--- app/routes.js
+++ app/routes.js
@@ -17,7 +17,7 @@ module.exports = function (app) {
 	app.get('/api/search', function (req, res) {
   
 	  const options = {
-		mirror: 'http://libgen.io',
+		mirror: 'http://libgen.is',
 		query: 'lord of the ring',
 		count: 30
 	  };
