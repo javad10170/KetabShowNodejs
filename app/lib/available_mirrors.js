0da7c14f8c30edda2c4eead55074e1cfcfcd3b9b
javad gholampoor <javad10170@hotmail.com>
2019-09-23 15:22:05 +0330

    Update available_mirrors.js

--- app/lib/available_mirrors.js
+++ app/lib/available_mirrors.js
@@ -4,16 +4,6 @@ module.exports = [
     canDownloadDirect: false
   },
   {
-    baseUrl: 'http://185.39.10.101',
-    // if true, '/get.php?md5=' works
-    canDownloadDirect: true
-  },
-  {
-    baseUrl: 'http://libgen.lc',
-    // if true, '/get.php?md5=' works
-    canDownloadDirect: true
-  },
-  {
     baseUrl: 'http://libgen.li',
     // if true, '/get.php?md5=' works
     canDownloadDirect: true
